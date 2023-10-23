29d933a0aba93d32b9a635fe2e705d7b878411fd
Dermot Hardy <dhardy2@opentext.com>
2023-10-23 11:00:00 +0100

    Import updates

--- worker-taskstowing/src/main/java/com/github/jobservice/workers/taskstowing/factory/TaskStowingWorkerConfiguration.java
+++ worker-taskstowing/src/main/java/com/github/jobservice/workers/taskstowing/factory/TaskStowingWorkerConfiguration.java
@@ -16,10 +16,10 @@
 package com.github.jobservice.workers.taskstowing.factory;
 
 import com.hpe.caf.api.worker.WorkerConfiguration;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public final class TaskStowingWorkerConfiguration extends WorkerConfiguration
 {

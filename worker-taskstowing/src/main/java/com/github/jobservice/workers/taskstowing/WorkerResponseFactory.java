572bd27a62b31a8b4bc1bfe3bd686a805109204c
Raghavendran Gopalakrishnan <rgopalakris3@opentext.com>
2024-11-18 18:11:25 +0530

    US975260: Refactor to remove HPE references in package names
    https://internal.almoctane.com/ui/entity-navigation?p=131002/6001&entityType=work_item&id=975260

--- worker-taskstowing/src/main/java/com/github/jobservice/workers/taskstowing/WorkerResponseFactory.java
+++ worker-taskstowing/src/main/java/com/github/jobservice/workers/taskstowing/WorkerResponseFactory.java
@@ -15,11 +15,11 @@
  */
 package com.github.jobservice.workers.taskstowing;
 
-import com.hpe.caf.api.Codec;
-import com.hpe.caf.api.CodecException;
-import com.hpe.caf.api.worker.TaskFailedException;
-import com.hpe.caf.api.worker.TaskStatus;
-import com.hpe.caf.api.worker.WorkerResponse;
+import com.github.cafapi.common.api.Codec;
+import com.github.cafapi.common.api.CodecException;
+import com.github.workerframework.api.TaskFailedException;
+import com.github.workerframework.api.TaskStatus;
+import com.github.workerframework.api.WorkerResponse;
 import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
